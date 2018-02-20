24f498d4415b22739bd70a42800ef3d2af081215
heybluguy <heybluguy@yahoo.com>
2018-02-20 10:01:13 -0700

    1 test passing

--- lib/database.rb
+++ lib/database.rb
@@ -1,14 +1,18 @@
 
 class Database
+  attr_reader :violations
+
   def initialize(csv)
     @violations = recieve_violations(csv)
-
   end
   def recieve_violations(csv)
+    violations = []
     csv_file = File.open(csv, "r")
       csv_read = csv_file.readlines
       csv_file.close
-      @violations << csv_read
-      @violations = @violations.flatten drop(1)
+      violations << csv_read
+      violations = violations.flatten.drop(1)
   end
+
+
 end
--- test/database_test.rb
+++ test/database_test.rb
@@ -4,6 +4,10 @@ require 'minitest/pride'
 require './lib/database'
 
 class DatabaseTest < Minitest::Test
+  def test_should_consume_csv_file
+    @database = Database.new("./Violations-2012.csv")
 
+    assert_equal @database.violations.count, 545
+  end
 
 end
